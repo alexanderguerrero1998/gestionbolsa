008132bf99a04802fcf632eed65bb85c2001e156
Alexander Guerrero <alexanderxn3@gmail.com>
2023-04-17 12:00:36 -0500

    funciona devexpress

--- Unach.DA.Empleo.CentralAdmin/Unach.DA.Empleo.Presentacion.CentralAdmin/Reportes/XtraReport1.cs
+++ Unach.DA.Empleo.CentralAdmin/Unach.DA.Empleo.Presentacion.CentralAdmin/Reportes/XtraReport1.cs
@@ -1,9 +1,12 @@
+﻿using DevExpress.XtraReports.UI;
 using System;
-using DevExpress.XtraReports.UI;
+using System.Collections;
+using System.ComponentModel;
+using System.Drawing;
 
 namespace Unach.DA.Empleo.Presentacion.CentralAdmin.Reportes
 {
-    public partial class XtraReport1
+    public partial class XtraReport1 : DevExpress.XtraReports.UI.XtraReport
     {
         public XtraReport1()
         {
