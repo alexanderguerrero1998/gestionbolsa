f30f759902581b5e6d4f7952e281c870c346742d
Alexander Guerrero <alexanderxn3@gmail.com>
2023-05-04 15:41:00 -0500

    Registra cuentas

--- Unach.DA.Empleo.CentralAdmin/Unach.DA.Empleo.Presentacion.CentralAdmin/Program.cs
+++ Unach.DA.Empleo.CentralAdmin/Unach.DA.Empleo.Presentacion.CentralAdmin/Program.cs
@@ -1,5 +1,6 @@
 using AutoMapper;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Unach.DA.Empleo.Persistencia.Core.Models;
 using Unach.DA.Empleo.Presentacion.CentralAdmin.Data;
@@ -13,8 +14,6 @@ namespace Unach.DA.Empleo.Presentacion.CentralAdmin
         {
             var builder = WebApplication.CreateBuilder(args);
 
-
-
             // Automapper
             var mappingConfig = new MapperConfiguration(mc =>
             {
@@ -23,17 +22,45 @@ namespace Unach.DA.Empleo.Presentacion.CentralAdmin
             IMapper mapper = mappingConfig.CreateMapper();
             builder.Services.AddSingleton(mapper);
 
+            builder.Services.AddDbContext<SicoaContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+            builder.Services.AddDbContext<IdentityDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+            builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+            {
+                options.Password.RequiredLength = 5;
+                options.Password.RequiredUniqueChars = 1;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireDigit = false;
+                options.SignIn.RequireConfirmedAccount = false;
+            })
+            .AddRoles<IdentityRole>()
+            .AddEntityFrameworkStores<IdentityDbContext>();
+
+
+            //builder.Services.Configure<IdentityOptions>(options =>
+            //{
+            //    options.SignIn.RequireConfirmedAccount = true;
+            //});
+
+
+            // builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount).AddEntityFrameworkStores<SicoaContext>();
 
             // Add services to the container.
-            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-            builder.Services.AddDbContext<SicoaContext>(options =>
-                options.UseSqlServer(connectionString));
-            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+            // var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            // builder.Services.AddDbContext<SicoaContext>(options => options.UseSqlServer(connectionString));
+            //builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+            //builder.Services.AddDefauIdentity.ApplicationltIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<SicoaContext>();
+            //builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<SicoaContext>();//<-
 
-            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<SicoaContext>();
-            builder.Services.AddControllersWithViews();
+            //  builder.Services.AddControllersWithViews();
+            //  builder.Services.AddRazorPages(); //<-
+            //  builder.Services.Configure<IISServerOptions>(options =>
+            //  {
+            //      options.AllowSynchronousIO = true;
+            //  });
 
+            //  builder.Services.AddDevExpressControls();
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -53,6 +80,7 @@ namespace Unach.DA.Empleo.Presentacion.CentralAdmin
 
             app.UseRouting();
 
+            app.UseAuthentication(); ///<-
             app.UseAuthorization();
 
             app.MapControllerRoute(
@@ -61,6 +89,7 @@ namespace Unach.DA.Empleo.Presentacion.CentralAdmin
             app.MapRazorPages();
 
             app.Run();
+
         }
     }
 }
